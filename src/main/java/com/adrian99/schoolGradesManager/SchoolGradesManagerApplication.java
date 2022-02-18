f4f0373d02b86664ba5a6043540ac5bad00cf1b4
adrianb1999 <blanariu_adrian@yahoo.com>
2022-02-18 13:47:20 +0200

    First commit

---
+++ src/main/java/com/adrian99/schoolGradesManager/SchoolGradesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.adrian99.schoolGradesManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolGradesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolGradesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrian99/schoolGradesManager/SchoolGradesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian99.schoolGradesManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolGradesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
